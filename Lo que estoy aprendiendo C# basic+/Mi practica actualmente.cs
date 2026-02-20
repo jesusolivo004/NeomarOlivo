bbb57b30b8ff1078db096531523d1e901cc309a9
jesusolivo004 <jesusolivo004@gmail.com>
2026-02-20 12:57:26 -0400

    update Mi practica actualmente.cs

---
+++ Lo que estoy aprendiendo C# basic+/Mi practica actualmente.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Jesus{
+    
+    class Esquelet
+    {
+        public static void Primero()
+        {
+          int vida = 
+        }
+    }
+    public class Program
+    {
+        static void Main ()
+        {
+            
+            Console.WriteLine("hoola");
+            Esquelet.Primero();
+        }
+    }
+}
